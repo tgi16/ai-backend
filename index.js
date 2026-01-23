06e8126596604a921c38edadb1ace4d47402688f
wypstudio <saifree1985@gmail.com>
2026-01-23 08:39:23 +0630

    Add GOOGLE_API_KEY environment variable

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 const GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
+const apiKey = process.env.GOOGLE_API_KEY;
 
 app.post('/api/generate', async (req, res) => {
   try {
