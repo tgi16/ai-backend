cec411226a6de9d332a6488ed6f5cffb5a0d3a12
wypstudio <saifree1985@gmail.com>
2026-01-23 10:44:38 +0630

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,10 +1,9 @@
 export default async function handler(req, res) {
-  // CORS headers
+  // CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Handle preflight request
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
