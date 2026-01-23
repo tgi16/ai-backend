1e1967d3ad3a7ff26c6ecf97b367b175799634cb
wypstudio <saifree1985@gmail.com>
2026-01-23 13:29:18 +0630

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  // CORS
+  // CORS (Browser ကနေ ချိတ်ဆက်နိုင်ဖို့)
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
