bd3ee46e4b9bc95091bc27d39521db32b764002f
wypstudio <saifree1985@gmail.com>
2026-01-24 17:03:01 +0630

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -29,7 +29,8 @@ export default async function handler(req, res) {
   }
 
   const r = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`,
+
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
